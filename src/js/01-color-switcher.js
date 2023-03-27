32501cea32f55d7d6e236abe1bfab72b3fdb4c58
VlasiukOleg <ikslprint@gmail.com>
2023-03-27 15:46:43 +0300

    made the  task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let setIntervalId = null;
+
+startBtn.addEventListener('click', onStartChangeRandomColor);
+stopBtn.addEventListener('click', onStopChangeRandomColor);
+
+function onStartChangeRandomColor(event) {
+  const isDisabled = false;
+
+  if (!isDisabled) {
+    event.target.setAttribute('disabled', '');
+  }
+
+  stopBtn.removeAttribute('disabled');
+
+  setIntervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopChangeRandomColor() {
+  clearInterval(setIntervalId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', '');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
