72fe4dfda1394cab96f37d782eda1985706070e6
VlasiukOleg <ikslprint@gmail.com>
2023-03-30 09:50:41 +0300

    finished Hw-09

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = formEl;
+
+  let delayStep = Number(delay.value);
+
+  for (let i = 1; i <= amount.value; i += 1) {
+    let position = i;
+
+    createPromise(position, delayStep)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delayStep += Number(step.value);
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
